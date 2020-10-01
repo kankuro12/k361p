5b7b9c2e444e2f8e7a129f5653c9fc00390e64ee
krishna5816 <krishna5816@github.com>
2020-10-01 16:06:03 +0545

    title changed

--- views/front/app.blade.php
+++ views/front/app.blade.php
@@ -2,67 +2,69 @@
 <html lang="en-ne">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta charset="UTF-8">
+  <meta name="description" content="">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-    <link rel="manifest" href="/manifest.json" />
+  <link rel="manifest" href="/manifest.json" />
 
-    <!-- Title -->
-    <title>@yield('title')</title>
+  <!-- Title -->
+  <title>Pro Peace Nepal</title>
 
-    <!-- Favicon -->
-    <link rel="icon" href="/assets/front/img/core-img/">
+  <!-- Favicon -->
+  <link rel="icon" href="/assets/front/img/core-img/">
 
-    <!-- Stylesheet -->
-    <link rel="stylesheet" href="/assets/front/style.css">
+  <!-- Stylesheet -->
+  <link rel="stylesheet" href="/assets/front/style.css">
 
 </head>
 
 <body>
-@include('front.header')
+  @include('front.header')
 
-<div class="">
-  @yield('content')
-</div>
+  <div class="">
+    @yield('content')
+  </div>
 
 
 
-@include('front.footer')
-<script src="/assets/front/js/jquery/jquery-2.2.4.min.js"></script>
-    <!-- Popper js -->
-    <script src="/assets/front/js/bootstrap/popper.min.js"></script>
-    <!-- Bootstrap js -->
-    <script src="/assets/front/js/bootstrap/bootstrap.min.js"></script>
-    <!-- All Plugins js -->
-    <script src="/assets/front/js/plugins/plugins.js"></script>
-    <!-- Active js -->
-    <script src="/assets/front/js/active.js"></script>
-    <script src="/assets/common/js/axios.js"></script>
-    
+  @include('front.footer')
+  <script src="/assets/front/js/jquery/jquery-2.2.4.min.js"></script>
+  <!-- Popper js -->
+  <script src="/assets/front/js/bootstrap/popper.min.js"></script>
+  <!-- Bootstrap js -->
+  <script src="/assets/front/js/bootstrap/bootstrap.min.js"></script>
+  <!-- All Plugins js -->
+  <script src="/assets/front/js/plugins/plugins.js"></script>
+  <!-- Active js -->
+  <script src="/assets/front/js/active.js"></script>
+  <script src="/assets/common/js/axios.js"></script>
 
-    @yield('script')
 
-    <script type="text/javascript">
-function googleTranslateElementInit() {
+  @yield('script')
+
+  <script type="text/javascript">
+    function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
-</script>
+  </script>
 
-<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+  <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+  </script>
 
-@if(isset($_SESSION['auth']))
-<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
-<script>
-  var OneSignal = window.OneSignal || [];
+  @if(isset($_SESSION['auth']))
+  <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+  <script>
+    var OneSignal = window.OneSignal || [];
   OneSignal.push(function() {
     OneSignal.init({
       appId: "f5d59766-3218-4ae4-8eab-df971c5f52b4",
     });
   });
-</script>
-@endif
+  </script>
+  @endif
 </body>
+
 </html>
